531d6de9ba0b91b91110fca2b681127a1df24bef
Karthigeswaran Arumugam <karthigeswaran1995@gmail.com>
2020-08-13 12:09:18 +0530

    First Stage

---
+++ notepad.java
@@ -0,0 +1,17 @@
+package Notepad;
+
+import java.util.Scanner;
+
+public class notepad {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        menu:do{
+            System.out.println("Notepad Application");
+            System.out.println("Options:");
+            System.out.println("1.New file");
+            System.out.println("1.New file");
+            System.out.println("1.New file");
+            System.out.println("1.New file");
+        }while(true)
+    }
+}
